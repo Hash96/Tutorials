71320ad49c2c27b4fa4a973dea410f4fc2d03520
Hash96 <hmhmkumara@students.nsbm.lk>
2020-06-27 06:21:52 +0530

    adding simple tasks

---
+++ project5/project5/ConvertValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project5
+{
+    class ConvertValues
+    {
+        public void KilometerToMeter()
+        {
+            int km;
+            Console.WriteLine("enter the km value");
+            km = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("converted  value is " + (km * 1000)+" meters");
+        }
+    }
+}
---
+++ project5/project5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertValues cv = new ConvertValues();
+            cv.KilometerToMeter();
+
+            Console.ReadKey();
+        }
+    }
+}
