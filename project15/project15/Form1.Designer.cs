17c84dd8ffab43f2f3c29374b472028752ed5d31
Hash96 <hmhmkumara@students.nsbm.lk>
2020-07-01 08:31:45 +0530

    adding simple c# projects

---
+++ project15/project15/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace project15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(109, 106);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(66, 17);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Number1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(109, 232);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(66, 17);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Number2";
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(259, 101);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(100, 22);
+            this.num1.TabIndex = 2;
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(259, 227);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(100, 22);
+            this.num2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(256, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "summation is here";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(283, 392);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Add";
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
