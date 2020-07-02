80d200b4b9670a82830113e77554703f86343520
Hash96 <hmhmkumara@students.nsbm.lk>
2020-07-02 07:46:52 +0530

    simple c# project

---
+++ project17/project17/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace project17
+{
+    partial class btn1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(543, 166);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(97, 17);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Content Value";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(535, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click Me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btn1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1176, 602);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl1);
+            this.Name = "btn1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Btn1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ project17/project17/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project17
+{
+    public partial class btn1 : Form
+    {
+        int count = 0;
+        public btn1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            lbl1.Text = count.ToString();
+        }
+
+        private void Btn1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
