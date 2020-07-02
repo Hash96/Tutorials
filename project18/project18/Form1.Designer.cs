c332dcf9da93fba780f03b20f23f92674e322e21
Hash96 <hmhmkumara@students.nsbm.lk>
2020-07-02 07:48:35 +0530

    simple C# project

---
+++ project18/project18/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace project18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(525, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(118, 190);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(73, 17);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Username";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(118, 380);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(69, 17);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "Password";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(369, 190);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 22);
+            this.txt1.TabIndex = 3;
+            // 
+            // txt2
+            // 
+            this.txt2.HideSelection = false;
+            this.txt2.Location = new System.Drawing.Point(369, 375);
+            this.txt2.Name = "txt2";
+            this.txt2.PasswordChar = '*';
+            this.txt2.Size = new System.Drawing.Size(100, 22);
+            this.txt2.TabIndex = 4;
+            this.txt2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(369, 521);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(75, 23);
+            this.login.TabIndex = 5;
+            this.login.Text = "Login";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 596);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button login;
+    }
+}
+
---
+++ project18/project18/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            String username = txt1.Text;
+            String password = txt2.Text;
+
+
+            if (username == "Admin" && password == "admin123")
+            {
+                Form2 fm = new Form2();
+                fm.Show();
+                this.Hide();
+            }
+            else { }
+        }
+    }
+}
